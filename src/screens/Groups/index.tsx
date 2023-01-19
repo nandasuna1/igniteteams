47d9fb23c9784c2711ba54b35ad34b5da5ed55de
Fernanda Pinho <fbp@ecomp.poli.br>
2023-01-19 00:08:24 -0300

    feat: newGroups screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,40 @@
+import { useState } from 'react';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlitghs } from '@components/Highlights';
 
 import { Container } from './styles';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/EmptyList';
+import { Button } from '@components/Button';
 
 
 
 export function Groups() {
+  const [groups, setGroups] = useState([]);
+
+
   return (
     <Container>
       <Header />
       <Highlitghs title='Turmas' subtitle='Jogue com suas turmas'/>
-
-      <GroupCard title='Galera do Ignite'/>
+      
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <GroupCard
+          title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message='Que tal cadastrar a primeira turma?'
+          />
+        )}
+      />
+      <Button title='Criar nova turma'/>
     </Container>
   );
 }
---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@components/Button";
+import { Header } from "@components/Header";
+import { Highlitghs } from "@components/Highlights";
+import { Input } from "@components/Input";
+import { Container, Content, Icon } from "./styles";
+
+export function NewGroup (){
+    return(
+        <Container>
+            <Header showBackButton/>
+            <Content>
+                <Icon />
+                <Highlitghs 
+                    title='Nova Turma'
+                    subtitle="Crie a turma para adicionar as pessoas"
+                />
+                <Input/>
+                <Button title="Criar" style={{marginTop: 20}}/>
+            </Content>
+        </Container>
+    )
+}
